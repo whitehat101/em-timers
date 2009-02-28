b801797911457362099e398ea5881f190f539cc0
Jake Douglas <jakecdouglas@gmail.com>
2009-02-27 21:34:13 -0800

    can now pass a number of seconds as :starting

--- lib/em-cron/em-cron.rb
+++ lib/em-cron/em-cron.rb
@@ -1,11 +1,11 @@
 module EventMachine
-    
+
   def self.do(options={}, &blk)
     increment = options[:every]
     starting = options[:starting]
-    
+
     reschedule = proc {|inc, block| EM.add_timer(inc) { reschedule.call(inc, block) } ; block.call }
-    
+
     if starting == :now
       EM.add_timer(increment) { reschedule.call(increment, blk) }
       blk.call
@@ -13,37 +13,41 @@ module EventMachine
     else
       if starting.is_a?(String)
         starting = Object.const_defined?("Chronic") ? Chronic.parse(starting) : Time.parse(starting)
-      elsif starting.is_a?(Time)
+      elsif starting.is_a?(Time) || starting.kind_of?(Numeric)
         starting = starting
       else
         starting = Time.now
       end
     end
 
-    time = starting - Time.now
-    while time < 0
-      time += increment
+    if starting.is_a?(Time)
+      time = starting - Time.now
+      while time < 0
+        time += increment
+      end
+    else
+      time = starting
     end
 
     EM.add_timer(time) { reschedule.call(increment, blk) }
-    
+
     return nil
   end
-  
+
   def self.do_hourly(options={}, &blk)
     self.do({:every => 1.hour}.merge(options), &blk)
   end
-  
+
   def self.do_daily(options={}, &blk)
     self.do({:every => 1.day}.merge(options), &blk)
   end
-  
+
   def self.do_weekly(options={}, &blk)
     self.do({:every => 1.week}.merge(options), &blk)
   end
-  
+
   def self.do_monthly(options={}, &blk)
     self.do({:every => 1.month}.merge(options), &blk)
   end
-  
+
 end
